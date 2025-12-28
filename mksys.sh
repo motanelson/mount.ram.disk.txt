adc165d54133cef5c6d805bca03d7f4ee9baa9d2
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-12-28 17:10:37 +0000

    Add files via upload

--- mksys.sh
+++ mksys.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
 
-# verifica argumento
+# check 1 argument
 if [ -z "$1" ]; then
-    echo "Uso: $0 imagem.img"
+    echo "Use: $0 imagem.img"
     exit 1
 fi
 
 IMG="$1"
 SIZE_MB=12
 
-echo "[1] Criar imagem de ${SIZE_MB}MB..."
+echo "[1] create image de ${SIZE_MB}MB..."
 dd if=/dev/zero of="$IMG" bs=1M count=$SIZE_MB status=progress
 
-echo "[2] Formatar em FAT12..."
+echo "[2] Format em FAT12..."
 mkfs.fat -F 12 "$IMG"
 
-echo "[3] Instalar Syslinux..."
+echo "[3] Install Syslinux..."
 syslinux "$IMG"
 
-echo "[4] Criar syslinux.cfg..."
+echo "[4] create syslinux.cfg..."
 cat << EOF > syslinux.cfg
 DEFAULT kernel
 LABEL kernel
     KERNEL kernel.bin
 EOF
 
-echo "[5] Copiar ficheiros para a imagem..."
+echo "[5] copy files into image..."
 mcopy -i "$IMG" syslinux.cfg ::/syslinux.cfg
 
-# exemplo: kernel fictício
+# sample: kernel 
 echo "BOOT OK" > kernel.bin
 mcopy -i "$IMG" kernel.bin ::/kernel.bin
 
 rm kernel.bin syslinux.cfg
 
-echo "[OK] Imagem criada: $IMG"
+echo "[OK] Image copy: $IMG"
